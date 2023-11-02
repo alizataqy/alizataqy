46a72a47491ec183c36797b16dbf94810050ee26
alizataqy <farizbushy@gmail.com>
2023-11-02 09:53:54 +0700

    new js

--- checkbox/kiw.js
+++ checkbox/kiw.js
@@ -16,3 +16,29 @@ $(document).ready(function() {
         alert('Registration successful!');
     });
 });
+
+$(function() {
+    $( "#button" ).click(function() {
+      $( "#button" ).addClass( "onclic", 250, validate);
+    });
+  
+    function validate() {
+      setTimeout(function() {
+        $( "#button" ).removeClass( "onclic" );
+        $( "#button" ).addClass( "validate", 450, callback );
+      }, 2250 );
+    }
+      function callback() {
+        setTimeout(function() {
+          $( "#button" ).removeClass( "validate" );
+        }, 1250 );
+      }
+    });
+
+    const circle = document.querySelector('.circle');
+
+window.addEventListener('mousemove', handleCursor);
+
+function handleCursor(e) {
+  circle.style.translate = `${e.x - (circle.offsetWidth/ 2)}px ${e.y - (circle.offsetHeight / 2)}px` 
+}
